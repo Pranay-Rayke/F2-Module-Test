433fca1de2909abd74130f0397f84092fdf332b4
Pranay Rayke <pranayrayke@gmail.com>
2023-09-03 18:27:32 +0530

    F2-Module Test

--- script.js
+++ script.js
@@ -1,72 +1,93 @@
-const fetchDataBtn = document.querySelector("#fbtn");
-
-const mapView = document.querySelector(".map");
-const detailsData = document.querySelector(".data");
-const locationData = document.querySelector(".locationData");
-const api = "c498aad05e6a6363d0f2d7a03437d474";
-
-async function getData(lat, long) {
-  const promise = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&exclude=current&appid=${api}`
-  );
-  return await promise.json();
-}
-
-async function gotLocation(position) {
-  const result = await getData(
-    position.coords.latitude,
-    position.coords.longitude
-  );
-  console.log(result);
-  detailData(result);
-}
-
-function failedLocation() {
-  console.log("There was Some issue");
-}
-
-fetchDataBtn.addEventListener("click", async () => {
-  navigator.geolocation.getCurrentPosition(gotLocation, failedLocation);
+//Constants
+const apiKey = "5c1d12e38eae18d3ab142ef3a2751417";
+const fetchBtn = document.getElementById("fetchBtn");
+const displayLocationPage = document.getElementById("displayLocationPage");
+const landingPage = document.getElementById("landingPage");
+const locationLats = document.getElementById("locationLats");
+const locationLongs = document.getElementById("locationLongs");
+const locationFrame = document.getElementById("locationFrame");
+const baseURL = `https://api.openweathermap.org/data/2.5/weather?`;
+fetchBtn.addEventListener("click", () => {
+  landingPage.classList.toggle("inactive");
+  displayLocationPage.classList.toggle("inactive");
+  getWeatherData();
 });
 
-function removeDom() {
-  document.querySelector(".first").remove();
+// Geting User Geolocation
+const getUserLocation = () => {
+  //Browser Geolocation Support Check
+  if ("geolocation" in navigator) {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lng = position.coords.longitude;
+          const location = { lat: lat, lng: lng };
+          resolve(location);
+        },
+        (error) => {
+          reject(`Error getting user location: ${error}`);
+        }
+      );
+    });
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+    return null;
+  }
+};
+
+function getWindDir(windDirection) {
+  let direction = parseInt(windDirection);
+  if (direction <= 20) return "North";
+  if (direction <= 70) return "North East";
+  if (direction <= 110) return "East";
+  if (direction <= 160) return "South East";
+  if (direction <= 210) return "South";
+  if (direction <= 250) return "South West";
+  if (direction <= 290) return "West";
+  if (direction <= 340) return "North West";
+  if (direction <= 360) return "North";
 }
 
-function detailData(result) {
-  removeDom();
-  const url = `https://maps.google.com/maps/?q=${result.coord.lat},${result.coord.lon}&output=embed`;
-  locationData.innerHTML = `
-  <div class="top">
-          <h1>Welcome To The Weather App</h1>
-          <p>Here is your current location</p>
-          <div class="latlong">
-            <p>Lat:<span class="lat">${result.coord.lat}</span></p>
-            <p>Long:<span class="long"> ${result.coord.lon}</span></p>
-          </div>
-          <div class="map" id="map">
-          <iframe
-          src=${url}
-          width="360"
-          height="270"
-          frameborder="0"
-          style="border:0;width: 90vw;
-          height: 65vh;margin-top:3rem; border-radius:1rem"></iframe>
-          </div>
-        </div>
-  <div class="down">
-          <div>
-            <h2>Your Weather Data</h2>
-          </div>
-          <div class="data">
-          <p>Location:${result.name}</p>
-            <p>Wind Speed:${result.wind.speed} kmph</span></p>
-            <p>Humidity:${result.main.humidity}</p>
-            <p>Time Zone:${result.timezone}</p>
-            <p>Pressure:${result.main.pressure} bar</p>
-            <p>Wind Direction:${result.wind.deg}</p>
-            <p>Feels like:${result.main.feels_like}</p>
-          </div>
-        </div>
-    `;
+function getTimeZone(second) {
+  let seconds = parseInt(second);
+  const hours = Math.floor(Math.abs(seconds) / 3600);
+  const minutes = Math.floor((Math.abs(seconds) % 3600) / 60);
+  const sign = seconds < 0 ? "-" : "+";
+  const timeZone = `GMT ${sign}${String(hours).padStart(2, "0")}:${String(
+    minutes
+  ).padStart(2, "0")}`;
+  return timeZone;
+}
+
+function addWeatherDataOnUI(location) {
+  document.getElementById("city").innerHTML += ' '+ location.name;
+  document.getElementById("windSpeed").innerHTML += ' '+
+    location.wind.speed + ` kmph`;
+  document.getElementById("humidity").innerHTML += ' '+ location.main.humidity;
+  document.getElementById("timeZone").innerHTML += ' '+ getTimeZone(
+    location.timezone
+  );
+  document.getElementById("pressure").innerHTML += ' '+ (location.main.pressure*100) * 1.01325e-5 + ` atm`;
+  document.getElementById("windDir").innerHTML += ' '+ getWindDir(location.wind.deg);
+  // document.getElementById('uvIndex').innerHTML += location.name;
+  document.getElementById("feelsLike").innerHTML += ' '+ `${Math.round(
+    location.main.feels_like - 273.15
+  )}&deg C`;
+}
+
+async function getWeatherData() {
+  try {
+    const getLocation = await getUserLocation();
+    locationLongs.innerHTML += getLocation.lng;
+    locationLats.innerHTML += getLocation.lat;
+    const locationSrc = `https://maps.google.com/maps?q=${getLocation.lat}, ${getLocation.lng}&z=15&output=embed`;
+    locationFrame.setAttribute("src", locationSrc);
+    const URL = `${baseURL}lat=${getLocation.lat}&lon=${getLocation.lng}&appid=${apiKey}`;
+    const resonse = await fetch(URL, { method: "GET" });
+    const result = await resonse.json();
+    addWeatherDataOnUI(result);
+  } catch (error) {
+    console.log(error);
+  }
 }
